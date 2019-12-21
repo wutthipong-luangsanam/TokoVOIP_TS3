f70a1f13cf4ba0c7494a2eba040d6dbbad6003cf
CryptoGenics <45253987+CryptoGenics@users.noreply.github.com>
2019-12-21 12:18:17 -0600

    Update c_main.lua

--- fivem_script/tokovoip_script/src/c_main.lua
+++ fivem_script/tokovoip_script/src/c_main.lua
@@ -213,16 +213,16 @@ AddEventHandler("initializeVoip", function()
 					pos_y = 1.1 + (math.ceil(i/12) * 0.1);
 					pos_x = 0.60 + ((i - (12 * math.floor(i/12)))/15);
 
-					drawTxt(pos_x, pos_y, 1.0, 1.0, 0.2, "[" .. playerServerId .. "] " .. GetPlayerName(player) .. "\nMode: " .. tostring(getPlayerData(playerServerId, "voip:mode")) .. "\nChannel: " .. tostring(getPlayerData(playerServerId, "radio:channel")) .. "\nRadioTalking: " .. tostring(getPlayerData(playerServerId, "radio:talking")) .. "\npluginStatus: " .. tostring(getPlayerData(playerServerId, "voip:pluginStatus")) .. "\npluginVersion: " .. tostring(getPlayerData(playerServerId, "voip:pluginVersion")) .. "\nTalking: " .. tostring(getPlayerData(playerServerId, "voip:talking")), 255, 255, 255, 255);
+					DrawTxt( "[" .. playerServerId .. "] " .. GetPlayerName(player) .. "\nMode: " .. tostring(getPlayerData(playerServerId, "voip:mode")) .. "\nChannel: " .. tostring(getPlayerData(playerServerId, "radio:channel")) .. "\nRadioTalking: " .. tostring(getPlayerData(playerServerId, "radio:talking")) .. "\npluginStatus: " .. tostring(getPlayerData(playerServerId, "voip:pluginStatus")) .. "\npluginVersion: " .. tostring(getPlayerData(playerServerId, "voip:pluginVersion")) .. "\nTalking: " .. tostring(getPlayerData(playerServerId, "voip:talking")), pos_x, pos_y, 1.0, true, 161, 3, 0, 255, true)
 				end
 				local i = 0;
 				for channelIndex, channel in pairs(voip.myChannels) do
 					i = i + 1;
-					drawTxt(0.8 + i/12, 0.5, 1.0, 1.0, 0.2, channel.name .. "(" .. channelIndex .. ")", 255, 255, 255, 255);
+					DrawTxt(channel.name .. "(" .. channelIndex .. ")", 0.8 + i/12, 0.5, 1.0, true, 161, 3, 0, 255, true)
 					local j = 0;
 					for _, player in pairs(channel.subscribers) do
 						j = j + 1;
-						drawTxt(0.8 + i/12, 0.5 + j/60, 1.0, 1.0, 0.2, player, 255, 255, 255, 255);
+						DrawTxt(player, 0.8 + i/12, 0.5 + j/60, 1.0, true, 161, 3, 0, 255, true)
 					end
 				end
 			end
